624688b5f85ceb4927bb897f9c05540b577fe938
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-03-12 18:24:35 +0300

    some fixes

--- Assets/Scripts/PinController.cs
+++ Assets/Scripts/PinController.cs
@@ -31,7 +31,7 @@ public class PinController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Debug.Log(Mathf.Abs(transform.rotation.z));
     }
 
     public void RaiseIfStanding()
