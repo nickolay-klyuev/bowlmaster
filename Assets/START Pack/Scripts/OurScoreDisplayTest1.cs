a9018b3194e522262838cc382192429434ed057f
Nickolay Klyuev <nickolay.klyuev@gmail.com>
2021-03-19 12:41:45 +0300

    start creating score master

--- Assets/START Pack/Scripts/OurScoreDisplayTest1.cs
+++ Assets/START Pack/Scripts/OurScoreDisplayTest1.cs
@@ -1,17 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
-//using NUnit.Framework;
-//using UnityEngine;
-//using System.Linq;
-//
-//[TestFixture]
-//public class OurScoreDisplayTest {
-//	
-//	[Test]
-//	public void T00PassingTest () {
-//		Assert.AreEqual (1, 1);
-//	}
-//
+/* using NUnit.Framework;
+using UnityEngine;
+using System.Linq;
+
+[TestFixture]
+public class OurScoreDisplayTest {
+	
+	[Test]
+	public void T00PassingTest () {
+		Assert.AreEqual (1, 1);
+	}
+ */
 //	[Test]
 //	public void T01Bowl1 () {
 //		int[] rolls = {1};
@@ -93,5 +93,5 @@ using System.Collections.Generic;
 //		string rollsString = "X X X X 9-X X X X X91";
 //		Assert.AreEqual (rollsString, ScoreDisplay.FormatRolls(rolls.ToList()));
 //	}
-//
+
 //}
--- Assets/Scripts/ScoreMaster.cs
+++ Assets/Scripts/ScoreMaster.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class ScoreMaster
 {
+    public static List<int> ScoreCumulative(List<int> rolls)
+    {
+        List<int> cumulativeScores = new List<int>();
+        int runningTotal = 0;
+
+        foreach(int frameScore in ScoreFrames(rolls))
+        {
+            runningTotal += frameScore;
+            cumulativeScores.Add(runningTotal);
+        }
+
+        return cumulativeScores;
+    }
+
     public static List<int> ScoreFrames(List<int> rolls)
     {
         List<int> frameList = new List<int>();
